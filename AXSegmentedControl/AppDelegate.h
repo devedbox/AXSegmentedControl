35585b880dc08f365dd8a957cb548e9d34f4bc12
aiXing <862099730@qq.com>
2016-05-31 09:55:44 +0800

    Initial Commit

---
+++ AXSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AXSegmentedControl
+//
+//  Created by ai on 16/5/31.
+//  Copyright © 2016年 devedbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
